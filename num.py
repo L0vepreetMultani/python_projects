eaee5add9f09e04d3a8d6bea37dfe7e8e4d9158b
lovepreet singh multani <lovepreetsingh9814@gmail.com>
2023-02-24 14:57:39 -0800

    second change in the file

--- num.py
+++ num.py
@@ -2,7 +2,10 @@
 import numpy as np
 import urllib.request
 # use url retrieve function of urllib.request url to add the data of that csv into the climate.txt file
+
 urllib.request.urlretrieve(
-    'https: // gist.github.com/BirajCoder/a4ffcb76fd6fb221d76ac2ee2b8584e9/raw/4054f90adfd361b7aa4255e99c2e874664094cea/climate.csv', 'climate.txt')
+    'https://gist.github.com/BirajCoder/a4ffcb76fd6fb221d76ac2ee2b8584e9/raw/4054f90adfd361b7aa4255e99c2e874664094cea/climate.csv',
+    'climate.txt')
 climate_data = np.genfromtxt('climate.txt', delimiter=',', skip_header=1)
+climate_data
 print(climate_data)
